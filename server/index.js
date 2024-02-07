779f9101d76d11beecfb687a80bd6ae20e6f6cfc
Himanshupachouri34 <146018456+Himanshupachouri34@users.noreply.github.com>
2024-02-07 19:42:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,14 @@ const Razorpay = require("razorpay")
 require('dotenv').config()
 const app = express()
 
+app.use(cors({
+    origin: "https://netflix-app-nine-delta.vercel.app/",
+    methods: ["POST", "GET"],
+    credentials: true
+}))
+
 app.use(express.json())
-app.use(cors())
+
 app.use(express.urlencoded({extended: false}))
 
 app.post("/order", async (req , res ) => {
